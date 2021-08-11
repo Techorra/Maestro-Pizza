367ab68d6828cd5bfe7e07be05b52c0656d3f671
Hamza-tech-maker-3 <75618822+Hamza-tech-maker-3@users.noreply.github.com>
2021-08-11 18:32:31 +0600

    fourth comment

--- src/components/MainPage/footer.js
+++ src/components/MainPage/footer.js
@@ -1,15 +1,18 @@
-import React from 'react'
+import React from "react";
 
 function footer() {
   return (
     <div>
-      <ul className="footer" style={{textAlign:"center", backgroundColor:"grey", padding:"20px" }}>
-
-        <li> copyright 2001</li>
-
+      <ul className="footer">
+        <p className="footer_items">
+          ©2020 MAESTRO PIZZINI | ALL RIGHTS RESERVED.
+          <span>
+            <img src="/img/logo.png" alt="logo" className="footer_logo" />
+          </span>
+        </p>
       </ul>
     </div>
-  )
+  );
 }
 
-export default footer
+export default footer;
